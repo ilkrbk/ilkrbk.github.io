8891cc74960d2c3f78ec6257331aa343413b425f
Ilya Korobka <ilkrbk@gmail.com>
2021-05-05 16:24:29 +0300

    test map

---
+++ map.js
@@ -0,0 +1,6 @@
+L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
+var map = L.mapquest.map('map', {
+    center: [39.7392, -104.9903],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 6
+});
